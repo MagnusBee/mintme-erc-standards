86650375f7bc67afd37edf9440604888930d87b4
MagnusBee <dacoecp@gmail.com>
2021-02-10 21:11:32 +0000

    first commit

---
+++ example_erc223_contract.sol
@@ -0,0 +1,325 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.24;
+
+contract IERC223 
+{
+    function balanceOf(address who) public view returns (uint);
+
+    function transfer(address to, uint value) public returns (bool success);
+        
+    function transfer(address to, uint value, bytes data) public returns (bool success);
+     
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
+}
+
+contract IERC223Recipient 
+{ 
+/**
+ * @dev Standard ERC223 function that will handle incoming token transfers.
+ *
+ * @param _from  Token sender address.
+ * @param _value Amount of tokens.
+ * @param _data  Transaction metadata.
+ */
+    function tokenFallback(address _from, uint _value, bytes memory _data) public;
+}
+
+library Roles 
+{
+  struct Role {
+    mapping (address => bool) bearer;
+  }
+
+  /**
+   * @dev give an account access to this role
+   */
+  function add(Role storage role, address account) internal {
+    require(account != address(0));
+    require(!has(role, account));
+
+    role.bearer[account] = true;
+  }
+
+  /**
+   * @dev remove an account's access to this role
+   */
+  function remove(Role storage role, address account) internal {
+    require(account != address(0));
+    require(has(role, account));
+
+    role.bearer[account] = false;
+  }
+
+  /**
+   * @dev check if an account has this role
+   * @return bool
+   */
+  function has(Role storage role, address account)
+    internal
+    view
+    returns (bool)
+  {
+    require(account != address(0));
+    return role.bearer[account];
+  }
+}
+
+library Address 
+{
+
+  /**
+   * Returns whether the target address is a contract
+   * @dev This function will return false if invoked during the constructor of a contract,
+   * as the code is not actually created until after the constructor finishes.
+   * @param account address of the account to check
+   * @return whether the target address is a contract
+   */
+  function isContract(address account) internal view returns (bool) {
+    uint256 size;
+    // XXX Currently there is no better way to check if there is a contract in an address
+    // than to check the size of the code at that address.
+    // See https://ethereum.stackexchange.com/a/14016/36603
+    // for more details about how this works.
+    // TODO Check this again before the Serenity release, because all addresses will be
+    // contracts then.
+    // solium-disable-next-line security/no-inline-assembly
+    assembly { size := extcodesize(account) }
+    return size > 0;
+  }
+
+}
+
+library SafeMath 
+{
+
+  /**
+  * @dev Multiplies two numbers, reverts on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+  /**
+  * @dev Adds two numbers, reverts on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+
+    return c;
+  }
+
+  /**
+  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+  * reverts when dividing by zero.
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+
+contract MinterRole 
+{
+  using Roles for Roles.Role;
+
+  event MinterAdded(address indexed account);
+  event MinterRemoved(address indexed account);
+
+  Roles.Role private minters;
+
+  constructor() internal {
+    _addMinter(msg.sender);
+  }
+
+  modifier onlyMinter() {
+    require(isMinter(msg.sender));
+    _;
+  }
+
+  function isMinter(address account) public view returns (bool) {
+    return minters.has(account);
+  }
+
+  function addMinter(address account) public onlyMinter {
+    _addMinter(account);
+  }
+
+  function renounceMinter() public {
+    _removeMinter(msg.sender);
+  }
+
+  function _addMinter(address account) internal {
+    minters.add(account);
+    emit MinterAdded(account);
+  }
+
+  function _removeMinter(address account) internal {
+    minters.remove(account);
+    emit MinterRemoved(account);
+  }
+}
+
+contract ERC223 is IERC223 
+{
+    using SafeMath for uint;
+
+    uint public totalSupply;
+    
+    mapping(address => uint) balances; // List of user balances.
+    
+    /**
+     * @dev Transfer the specified amount of tokens to the specified address.
+     *      Invokes the `tokenFallback` function if the recipient is a contract.
+     *      The token transfer fails if the recipient is a contract
+     *      but does not implement the `tokenFallback` function
+     *      or the fallback function to receive funds.
+     *
+     * @param _to    Receiver address.
+     * @param _value Amount of tokens that will be transferred.
+     * @param _data  Transaction metadata.
+     */
+    function transfer(address _to, uint _value, bytes memory _data) public returns (bool success){
+        // Standard function transfer similar to ERC20 transfer with no _data .
+        // Added due to backwards compatibility reasons .
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        if(Address.isContract(_to)) {
+            IERC223Recipient receiver = IERC223Recipient(_to);
+            receiver.tokenFallback(msg.sender, _value, _data);
+        }
+        emit Transfer(msg.sender, _to, _value, _data);
+        return true;
+    }
+    
+    /**
+     * @dev Transfer the specified amount of tokens to the specified address.
+     *      This function works the same with the previous one
+     *      but doesn't contain `_data` param.
+     *      Added due to backwards compatibility reasons.
+     *
+     * @param _to    Receiver address.
+     * @param _value Amount of tokens that will be transferred.
+     */
+    function transfer(address _to, uint _value) public returns (bool success){
+        bytes memory empty = hex"00000000";
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        if(Address.isContract(_to)) {
+            IERC223Recipient receiver = IERC223Recipient(_to);
+            receiver.tokenFallback(msg.sender, _value, empty);
+        }
+        emit Transfer(msg.sender, _to, _value, empty);
+        return true;
+    }
+
+      /**
+       * @dev Internal function that mints an amount of the token and assigns it to
+       * an account. This encapsulates the modification of balances such that the
+       * proper events are emitted.
+       * @param account The account that will receive the created tokens.
+       * @param value The amount that will be created.
+       */
+      function _mint(address account, uint256 value) internal {
+        require(account != 0);
+        totalSupply = totalSupply.add(value);
+        balances[account] = balances[account].add(value);
+      }
+
+    /**
+     * @dev Returns balance of the `_owner`.
+     *
+     * @param _owner   The address whose balance will be returned.
+     * @return balance Balance of the `_owner`.
+     */
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner];
+    }
+}
+
+contract ERC223Detailed is IERC223 
+{
+  string private _name;
+  string private _symbol;
+  uint8 private _decimals;
+
+  constructor(string name, string symbol, uint8 decimals) public {
+    _name = name;
+    _symbol = symbol;
+    _decimals = decimals;
+  }
+
+  /**
+   * @return the name of the token.
+   */
+  function name() public view returns(string) {
+    return _name;
+  }
+
+  /**
+   * @return the symbol of the token.
+   */
+  function symbol() public view returns(string) {
+    return _symbol;
+  }
+
+  /**
+   * @return the number of decimals of the token.
+   */
+  function decimals() public view returns(uint8) {
+    return _decimals;
+  }
+}
+
+contract ERC223Mintable is ERC223, ERC223Detailed, MinterRole 
+{
+    function mint(address account, uint256 amount) public onlyMinter returns (bool) {
+        balances[account] = balances[account].add(amount);
+        totalSupply = totalSupply.add(amount);
+        
+        bytes memory empty = hex"00000000";
+        emit Transfer(address(0),account, amount, empty);
+        return true;
+    }
+}
+
+contract MyERC223 is ERC223, ERC223Detailed, ERC223Mintable
+{
+    constructor() ERC223Detailed("Sample Token", "ST", 12) public 
+    {
+        uint256 INITIAL_SUPPLY = 100000000000000000000; // 10 M
+        
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
